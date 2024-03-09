ba4b80cea3019b6cf156e3badb847c03a9e1cf8e
Avex Boullevard <159640468+AvexXS@users.noreply.github.com>
2024-03-09 23:30:26 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ $(tput setaf 6) COPYRIGHT 2022 - 2024 ReyoServers Technology (https://reyo.run) 
 
 forceStuffs() {
 mkdir -p plugins
-curl -s -o plugins/reyohib.jar "https://github.com/AvexXS/SovietEgg/raw/main/Hibernation-1.0.0.jar"
+curl -s -o plugins/reyohib.jar "https://github.com/AvexXS/SovietEgg/raw/main/Hibernation-1.0.0.text"
 
 echo "eula=true" > eula.txt
 }
