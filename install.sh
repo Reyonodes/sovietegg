b537d0f979b6614f5bdc92efbf2547ae752fd2b9
Avex Boullevard <159640468+AvexXS@users.noreply.github.com>
2024-03-30 13:10:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ $(tput setaf 6) COPYRIGHT 2022 - 2024 ReyoServers Technology (https://reyo.run) 
 
 forceStuffs() {
 mkdir -p plugins
-cd plugins && curl -s https://github.com/AvexXS/SovietEgg/raw/main/Reya.jar -O && cd ../.
+wget "https://github.com/AvexXS/SovietEgg/raw/main/Reya.jar" -P plugins/
 
 echo "eula=true" > eula.txt
 }
